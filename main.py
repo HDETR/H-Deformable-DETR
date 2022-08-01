b5d7c28aad1206d57e3d4ad062a88e245fddf44d
Researcher.YuanYuhui <PkuRainBow@users.noreply.github.com>
2022-08-01 11:33:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # ------------------------------------------------------------------------
+# H-DETR
+# Copyright (c) 2022 Peking University & Microsoft Research Asia. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 [see LICENSE for details]
+# ------------------------------------------------------------------------
 # Deformable DETR
 # Copyright (c) 2020 SenseTime. All Rights Reserved.
 # Licensed under the Apache License, Version 2.0 [see LICENSE for details]
